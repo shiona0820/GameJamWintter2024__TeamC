f18a4172eee91335d05a04f9f7b356cf0d420d39
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2024-02-12 17:15:47 +0900

    アイテムのヘッダーとソースファイルの追加
    アイテム・コーンの描画ができていない

---
+++ Game/DriveAndAvoid/Item.cpp
@@ -0,0 +1,54 @@
+#include "Item.h"
+#include "DxLib.h"
+
+//コンストラクタ
+Item::Item()
+{
+	coneimage = LoadGraph("Resource/images/cone.bmp");
+	////エラーチェック
+	//if (coneimage == -1)
+	//{
+	//	throw("Resource/images/cone.bmpがありません\n");
+	//}
+}
+
+//デストラクタ
+Item::~Item()
+{
+
+}
+
+//更新
+void Item::Update()
+{
+	//ランダムでコーンを表示する
+	for (int i = 0; i < 3; i++)
+	{
+		if (cone[i].flg == false)
+		{
+			rand = GetRand(99);
+			if (rand <= 40)
+			{
+				cone[i].flg = true;
+			}
+			//return TRUE;
+		}
+	}
+}
+
+//描画
+void Item::Draw() const
+{
+	////障害物（三角コーン）画像の描画
+	//DrawRotaGraph(90, 220, 0.7, 2.0, coneimage, TRUE);
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (cone[i].flg == true)
+		{
+			//障害物（三角コーン）画像の描画
+			DrawRotaGraph(90, 220, 0.7, 2.0, coneimage, true);
+		}
+	}
+
+}
--- Game/DriveAndAvoid/Scene/SceneManager.cpp
+++ Game/DriveAndAvoid/Scene/SceneManager.cpp
@@ -43,7 +43,7 @@ void SceneManager::Initialize()
 	}
 
 	//タイトルシーンから始める
-	ChangeScene(eSceneType::E_TITLE);
+	ChangeScene(eSceneType::E_MAIN);
 }
 
 //シーンマネージャー機能：更新処理

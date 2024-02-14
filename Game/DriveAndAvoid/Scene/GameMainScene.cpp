af2a661f2b9d91d40ea29f1ed384ac1727c5f91e
sonan <yuka-sonan-k22h@stu.kbc.ac.jp>
2024-02-14 18:12:20 +0900

    komentosakujo

--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -317,50 +317,6 @@ void GameMainScene::Draw() const
 	//パトカーの描画
 	Pcar->Draw();
 
-	//アイテムの描画
-	//item->Draw();
-
-	////UIの描画
-	//DrawBox(500, 0, 640, 480, GetColor(0, 153, 0), TRUE);
-	//SetFontSize(16);
-	//DrawFormatString(510, 20, GetColor(0, 0, 0), "ハイスコア");
-	//DrawFormatString(560, 40, GetColor(255, 255, 255), "%08d", high_score);
-	//DrawFormatString(510, 80, GetColor(0, 0, 0), "避けた数");
-
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	DrawRotaGraph(523 + (i * 50), 120, 0.3, 0, enemy_image[i], TRUE, FALSE);
-	//	DrawFormatString(510 + (i * 50), 140, GetColor(255, 255, 255), "%03d", enemy_count[i]);
-	//}
-
-	//DrawFormatString(510, 200, GetColor(0, 0, 0), "走行距離");
-	//DrawFormatString(555, 220, GetColor(255, 255, 255), "%08d", mileage / 10);
-	//DrawFormatString(510, 240, GetColor(0, 0, 0), "スピード");
-	//DrawFormatString(555, 260, GetColor(255, 255, 255), "%08.1f", player->GetSpeed());
-
-	//DrawFormatString(200, 260, GetColor(255, 255, 255), "count%d", count);
-	//DrawFormatString(200, 280, GetColor(255, 255, 255), "timer%d", timer);
-
-	////バリア枚数の描画
-	//for (int i = 0; i < player -> GetBarriarCount(); i++)
-	//{
-	//	DrawRotaGraph(520 + i * 25, 340, 0.2f, 0, barrier_image, TRUE, FALSE);
-	//}
-
-	////燃料ゲージの描画
-	//float fx = 510.0f;
-	//float fy = 390.0f;
-	//DrawFormatString(fx, fy, GetColor(0, 0, 0), "FUEL METER");
-	//DrawBoxAA(fx, fy + 20.0f, fx + (player->GetFuel() * 100 / 20000), fy + 40.0f,
-	//	GetColor(0, 102, 204), TRUE);
-	//DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
-
-	////体力ゲージの描画
-	//fx = 510.0f;
-	//fy = 430.0f;
-	//DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "PLAYER HP");
-	//DrawBoxAA(fx, fy + 20.0f, fx + (player->GetHp() * 100 / 1000), fy + 40.0f,GetColor(255, 0, 0), TRUE);
-	//DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
 }
 
 //終了時処理

26d1722012e7a03365f3bd77ecdc8d1bf849dc29
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2024-02-15 13:48:45 +0900

    コメントの追加・いらないプログラムの削除
    ItemヘッダーとItem.cppファイルの使っていない変数や構造体の削除

--- Game/DriveAndAvoid/Item.cpp
+++ Game/DriveAndAvoid/Item.cpp
@@ -4,15 +4,13 @@
 //コンストラクタ
 Item::Item()
 {
+	//画像の読み込み
 	coneimage = LoadGraph("Resource/images/cone.bmp");
 
-	for (int i = 0; i < 3; i++)
-	{
-		cone[i].flg = FALSE;
-	}
-	gWaitTime = 100;
+	//locationの初期化
 	location.x = 0;
 	location.y = 0;
+	//当たり判定のボックスの設定
 	box_size = Vector2D(5.0f, 10.0f);
 }
 
@@ -24,6 +22,7 @@ Item::~Item()
 
 void Item::Initialize(int x)
 {
+	//GameMainSceneからx座標をとってくるための初期化
 	location.x = x*120;
 }
 
@@ -35,50 +34,15 @@ void Item::ResetY(float y)
 //更新
 void Item::Update()
 {
-	////ランダムでコーンを表示する
-	//for (int i = 0; i < ITEM_MAX; i++)
-	//{
-	//	if (cone[i].flg == false)
-	//	{
-	//		cone[i].x = (float)(GetRand(100) * 300 - 40);
-	//		if (cone[i].x<= 40)
-	//		{
-	//			cone[i].flg = true;
-	//		}
-	//		cone[i].x=1+(int)(rand()*(4-1+1));
-	//	}
-		//コーンを動かす
-		location.y++;
-		/*if (gWaitTime >= 700)
-		{
-			cone[i].x = (float)(GetRand(99) * 300 - 400);
-		}*/
-	//}
-	
+	//コーンを動かす(y座標)
+	location.y++;
 }
 
 //描画
 void Item::Draw() const 
 {
 	//障害物（三角コーン）画像の描画
-	//DrawRotaGraph(90, 220, 0.7, 0.0, coneimage, TRUE);
-
-	/*for(int j=0;j<gWaitTime;j++)
-	//{*/
-	//	for (int i = 0; i < ITEM_MAX; i++)
-	//	{
-	//		if (cone[i].flg == true /*|| count==i*/)
-	//		{
-				//障害物（三角コーン）画像の描画
-				DrawRotaGraph(location.x, 10 + location.y, 0.7, 0.0, coneimage, true);
-		//	}
-		//	//ある程度の座標に行ったら消える
-		//	if (location.y >= 725)
-		//	{
-		//		DeleteGraph(coneimage);
-		//	}
-		//}
-	//}
+	DrawRotaGraph(location.x, 10 + location.y, 0.7, 0.0, coneimage, true);
 }
 
 //位置情報を取得
